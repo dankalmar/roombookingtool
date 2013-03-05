836c12e4337b745bbf886f00b5606675bae40b2d
Heather Payne <heather@heatherpayne.ca>
2013-03-04 21:12:39 -0500

    New rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Roombookingapp::Application.config.secret_token = '865b45b7e75f40c904889801bc6fb631e4cfa21ad2d55471cd92b5340e68a435f41d4f9d28559ab3592c1639d6ecb789f77210fc97987d1def602093b53e0dae'
