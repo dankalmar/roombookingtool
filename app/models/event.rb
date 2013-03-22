3f76ff09713354bbe2c1d6f82b316c0c42f2ac90
dankalmar <dankalmar@gmail.com>
2013-03-21 21:22:57 -0400

    got everything working with date time picker

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,17 +1,14 @@
 class Event < ActiveRecord::Base
   attr_accessible :description, :time, :title, :start_date, :end_date, :start_time, :end_time
-
+  attr_accessor :start_date, :end_date, :start_time, :end_time
   belongs_to :user
 
-  def start_date
+  before_save :set_dates
+
+  def set_dates
+  	self.start_at = Time.zone.parse start_date + start_time
+  	self.end_at = Time.zone.parse end_date + end_time
+
   end
 
-  def start_time
-  end
-
-  def end_date
-  end
-
-  def end_time
-  end
 end
