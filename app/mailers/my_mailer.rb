82b1652d89f8b67c31726d9ac52a4e313060c3d7
Heather Payne <heather@heatherpayne.ca>
2013-04-08 20:37:28 -0400

    Beginning to add an event confirmation email

--- app/mailers/my_mailer.rb
+++ app/mailers/my_mailer.rb
@@ -8,4 +8,12 @@ class MyMailer < ActionMailer::Base
 			subject: 'Welcome')
 	end
 
+	def event_confirmation(user)
+		@user = user
+
+		mail(to: user.email,
+			from: 'dan@shoplocket.com'
+			subject: 'Your event is booked!')
+	end
+	
 end
