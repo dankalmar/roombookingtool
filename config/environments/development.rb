383a17a8cb31d0af108bf250c7f052751deefb58
Heather Payne <heather@heatherpayne.ca>
2013-04-21 14:34:28 -0400

    Setup email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,19 @@ Roombookingapp::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+  config.action_mailer.smtp_settings = {
+      :address              => "smtp.gmail.com",
+      :port                 => 587,
+      :user_name            => 'roombookingapp@gmail.com',
+      :password             => ENV['GMAIL_PASSWORD'],
+      :authentication       => 'plain',
+      :enable_starttls_auto => true  
+  }
+
+  config.action_mailer.default_url_options = {
+      :host => 'localhost',
+      :port => 3000
+  }
+
 end
