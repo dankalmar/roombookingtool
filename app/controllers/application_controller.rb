ac897789a543290f6bca1ccc7f9ed8ed2bc3ad3c
Heather Payne <heather@heatherpayne.ca>
2013-05-21 20:05:28 -0400

    More timezone fixes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  before_filter :set_user_time_zone
-  def set_user_time_zone
-    if user_signed_in? and current_user.time_zone
-      Time.zone = current_user.time_zone
-    end
-  end
-
 end
 
 
