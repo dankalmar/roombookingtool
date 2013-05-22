ecfc80c944e3808eeaaaef8dd52a8aa6bc565e19
Heather Payne <heather@heatherpayne.ca>
2013-05-21 20:12:43 -0400

    More timezone fixes

--- config/application.rb
+++ config/application.rb
@@ -37,7 +37,7 @@ module Roombookingapp
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
 
-    config.time_zone = 'Eastern Time (US & Canada)'
+    config.time_zone = 'UTC'
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
